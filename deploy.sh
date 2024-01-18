bf19feef5bcd7cac544d5201475a543f360262c2
Nick Myers <zenfootphilosopher@gmail.com>
2024-01-17 22:45:19 -0800

    Update website

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 git checkout gh-pages          # Switch to the gh-pages branch
 git pull origin gh-pages       # Ensure it's up-to-date with remote
 git rm -r *                    # Remove old files
-cp -r ../build/* .             # Copy new files from build directory
+cp -r ./build/* .             # Copy new files from build directory
 git add .                      # Add all files to staging
 git commit -m "Update website" # Commit changes
 git push origin gh-pages       # Push changes to remote
